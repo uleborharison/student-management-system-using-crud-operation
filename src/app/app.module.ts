9cd1de6c33e85dd43e7f59b1cd6d97e4a5bcae30
Harison <Uleborharison@gmail.com>
2022-11-02 14:02:41 +0100

    update student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentComponent } from './student/student.component';
-import { DepartmentComponent } from './department/department.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, StudentComponent, DepartmentComponent],
+  declarations: [AppComponent, StudentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
